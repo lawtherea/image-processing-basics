2ba42fe8c2a9f49e3ab1fe0ef164310a45a573c0
lawtherea <leilamariabiggi@outlook.com>
2026-01-10 15:02:19 -0300

    implemented a grayscale conversion function with weighted average

--- grayscale.py
+++ grayscale.py
@@ -16,7 +16,19 @@ def grayscale(colored_img):
             img.putpixel((x, y), (lum, lum, lum)) # sets the pixel to the corresponding grayscale color
     return img
 
+# but that's not how humans perceive brightness, so we use weighted average. our eyes are more sensitive to green light, then red light, then a little bit of blue, so we give weight.
+def grayscale_weighted(colored_img):
+    w, h = colored_img.size
+    img = Image.new("RGB", (w, h))
+
+    for x in range(w):
+        for y in range(h):
+            pxl = colored_img.getpixel((x, y))
+            lum = int(0.3 * pxl[0] + 0.59 * pxl[1] + 0.11 * pxl[2]) # weighted average
+            img.putpixel((x, y), (lum, lum, lum))
+    return img
+
 if __name__ == "__main__":
     cat = Image.open(in_path("cutecat.jpg"))
-    gray_cat = grayscale(cat)
+    gray_cat = grayscale_weighted(cat)
     gray_cat.save(out_path("cutecat_grayscale_manual.jpg"))
