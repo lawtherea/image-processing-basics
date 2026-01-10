f8d2010b45eb416afa89d2ff77d5bfdb207e474e
lawtherea <leilamariabiggi@outlook.com>
2026-01-10 17:40:42 -0300

    learning how to apply filters on images

---
+++ filters.py
@@ -0,0 +1,33 @@
+from utils import in_path, out_path
+from PIL import Image, ImageFilter
+import numpy as np
+
+# compare two images side by side
+def show_vertical(img1,img2):
+    im = Image.fromarray(np.hstack((np.array(img1), np.array(img2))))
+    im.show()
+
+def blur_image(input_image):
+    return input_image.filter(ImageFilter.GaussianBlur(radius=5))
+
+def contour_image(input_image):
+    return input_image.filter(ImageFilter.CONTOUR)
+
+def emboss_image(input_image):
+    return input_image.filter(ImageFilter.EMBOSS)
+
+if __name__ == "__main__":
+    img = Image.open(in_path("cutecat.jpg"))
+    blurred_img = blur_image(img)
+    # blurred_img.save(out_path("cutedog_blurred.jpg"))
+    show_vertical(img, blurred_img)
+
+    img2 = Image.open(in_path("cutecat.jpg"))
+    contoured_img = contour_image(img2)
+    # contoured_img.save(out_path("cutecat_contoured.jpg"))
+    show_vertical(img2, contoured_img)
+
+    img3 = Image.open(in_path("cutedog.jpg"))
+    embossed_img = emboss_image(img3)
+    # embossed_img.save(out_path("cutedog_embossed.jpg"))
+    show_vertical(img3, embossed_img)
